3eabe490a9faa862dc0e2e857f42d448ff92e902
mnalishuwa <mnalishuwa@gmail.com>
2023-08-10 12:26:35 +0000

    change lastdig to account for floor usage

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-rmd = number % 10
-if number < 0:
-    rmd *= -1
+
 # YOUR CODE HERE
+floor_ceil = number // 10
+
+# python uses the floor when it performs int division
+# therefore using modulo(%) can lead to undesired
+# results for negative numbers, because it uses
+# floor in calculating modulo in order for math ops
+# to remain consistent.
+
+# Therefore, we need to use the ceiling in calculating
+# the remainder when the number is negative
+if number < 0:
+    floor_ceil += 1  # result is ceiling if n < 0
+rmd = number - (floor_ceil * 10)
+
 if rmd == 0:
     print(f"Last digit of {number:d} is {rmd:d} and is 0")
 elif rmd > 5:
