a1940fa681c6f0b6a73d6fff2834ffac8b84e054
mnalishuwa <mnalishuwa@gmail.com>
2023-08-10 10:09:59 +0000

    change check cycle to O(n)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,34 +1,19 @@
 #include "lists.h"
 
-/**
- * check_cycle - checks if a linked list has a cycle
- * Description: traverses a singly linked list and checks
- * if the current not being visited has been visited before
- *
- * @list: pointer to the head of the list
- *
- * Return: 1 if cycle exists and 0 otherwise
- */
 int check_cycle(listint_t *list)
 {
-	listint_t *current_node = NULL, *temp = NULL;
-	int index, j;
+	listint *current_node = list, *next_node = list;
 
-	current_node = list;
-	index = 0;
-	while (current_node)
+	while (next_node != NULL)
 	{
 		current_node = current_node->next;
-		index += 1;
-		temp = list;
-		while (temp && j < index)
-		{
-			if (temp == current_node)
-			{
-				return (1);
-			}
-			j++;
-		}
+		next_node = next_node->next;
+
+		if (next_node == NULL)
+			return (0);
+		next_node = next_node->next;
+		if (next_node == current_node)
+			return (1);
 	}
 	return (0);
 }
