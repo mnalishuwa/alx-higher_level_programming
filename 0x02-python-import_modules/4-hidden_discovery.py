fd5a464a71dde3a70075594d764a6da821cbcaca
mnalishuwa <mnalishuwa@gmail.com>
2023-08-11 19:15:51 +0000

    fix hidden py typo

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     for each_name in dir(hidden_4):
-        if each_name[0] = "_":
+        if each_name[0] == "_":
             continue
         print("{}".format(each_name))
