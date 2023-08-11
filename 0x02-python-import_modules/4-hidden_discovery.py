ab6a9ff6acc44fb96aad68e5050d5fb9f696e38d
mnalishuwa <mnalishuwa@gmail.com>
2023-08-11 19:09:07 +0000

    update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,9 @@
 
 if __name__ == "__main__":
     import sys
-    
+    import hidden_4
+
+    for each_name in (dir(hidden_4)):
+        if each_name[0] = "_":
+            continue
+        print("{}".format(each_name))
