a3954c0d4d24bd98b0dc1374a1331fadbe01e0ae
mnalishuwa <mnalishuwa@gmail.com>
2023-08-11 18:20:35 +0000

    update only print args passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,14 @@
 if __name__ == "__main__":
     import sys
 
-    if len(sys.argv) == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
-    elif len(sys.argv) == 1:
+    elif len(sys.argv) == 2:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv)))
+        print("{} arguments:".format(len(sys.argv) - 1))
 
-    for arg_index in len(sys.argv):
-        print("{}: {}".format(arg_index + 1, sys.argv[arg_index]))
+    for arg_index in range(len(sys.argv)):
+        if arg_index == 0:
+            continue
+        print("{}: {}".format(arg_index, sys.argv[arg_index]))
