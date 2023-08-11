6e9befd15c5ed78499936e92413061ecaa79fba9
mnalishuwa <mnalishuwa@gmail.com>
2023-08-11 01:43:11 +0000

    update

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for ascii_int in range(122, 96, -1):
+    if ascii_int % 2 == 0:
+        res = ascii_int - 32
+    else:
+        res = ascii_int
+    print("{}".format(chr(res)), end="")
