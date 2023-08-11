85edc5c94dd5c14c1f82c04219acfac44f9add33
mnalishuwa <mnalishuwa@gmail.com>
2023-08-11 17:57:28 +0000

    fix pycode in calc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-import calculator_1 as calc
+if __name__ == "__main__":
+    import calculator_1 as calc
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-# print("{} + {} = {}".format(a, b, calc.add(a, b)))
-print(f"{a:d} + {b:d} = {calc.add(a, b):d}")
-print(f"{a:d} - {b:d} = {calc.sub(a, b):d}")
-print(f"{a:d} * {b:d} = {calc.mul(a, b):d}")
-print(f"{a:d} / {b:d} = {calc.div(a, b):d}")
+    # print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print(f"{a:d} + {b:d} = {calc.add(a, b):d}")
+    print(f"{a:d} - {b:d} = {calc.sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {calc.mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {calc.div(a, b):d}")
