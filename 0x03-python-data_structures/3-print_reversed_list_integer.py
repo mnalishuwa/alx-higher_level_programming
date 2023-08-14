ee25f38765e8236b262348412ddbe1c738c9de66
mnalishuwa <mnalishuwa@gmail.com>
2023-08-14 12:16:45 +0000

    update print reverse account for None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,7 @@
 
 def print_reversed_list_integer(my_list=[]):
 
-    size = len(my_list)
-    for idx in range(-1, -(size + 1), -1):
-        print("{:d}".format(my_list[idx]))
+    if my_list:
+        size = len(my_list)
+        for idx in range(-1, -(size + 1), -1):
+            print("{:d}".format(my_list[idx]))
