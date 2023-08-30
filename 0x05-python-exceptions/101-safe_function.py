71ce512574b6abad49acdc46a6d109514504f6b6
mnalishuwa <mnalishuwa@gmail.com>
2023-08-30 12:58:45 +0000

    Advanced

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as error_message:
+        print("Exception: {}\n".format(error_message))
+        return  None
