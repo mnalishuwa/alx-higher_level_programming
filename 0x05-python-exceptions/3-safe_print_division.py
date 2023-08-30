6ffe7c315dd7ccfc4df24c2d8c8dfd756fa10969
mnalishuwa <mnalishuwa@gmail.com>
2023-08-30 10:24:13 +0000

    safe_division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,5 +10,5 @@ def safe_print_division(a, b):
     except TypeError:
         result = None
     finally:
-        print("Inside Result: {}".format(result))
+        print("Inside result: {}".format(result))
         return result
