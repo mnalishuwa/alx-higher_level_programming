8995ba7e658183b6f278108d8bf41daf799ee504
mnalishuwa <mnalishuwa@gmail.com>
2023-08-30 03:11:19 +0000

    Update files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+
+
 """Square module Docs"""
 
 
 class Square:
-    """Square Class Base Template v0.1"""
+
+    """
+    Square Class Base Template v0.1
+    Attributes:
+    size (int)
+    """
     def __init__(self, size):
         """Contructor method, init instance variables"""
         self.size = size
