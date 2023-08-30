537741d69403b6e624c8b42d2fac94dbc4e539cb
mnalishuwa <mnalishuwa@gmail.com>
2023-08-30 13:04:54 +0000

    Advanced

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,9 +3,10 @@ from sys import stderr
 
 
 def safe_function(fct, *args):
+
     try:
         result = fct(*args)
         return result
     except Exception as error_message:
-        print("Exception: {}\n".format(error_message))
+        stderr.write("Exception: {}\n".format(error_message))
         return None
