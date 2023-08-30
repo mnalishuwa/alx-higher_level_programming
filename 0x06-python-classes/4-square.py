7435fcb441386943baf4ebc84cf3ca86fe11eddb
mnalishuwa <mnalishuwa@gmail.com>
2023-08-30 04:03:48 +0000

    Update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+"""Square module Docs"""
+
+
+class Square:
+    """
+    Square Class Base Template v0.2 - size validation
+
+    Attributes:
+        size (int): +ve square side
+    """
+    def __init__(self, size=0):
+        """
+        Contructor method, init instance variables
+        Args:
+            size: int
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Get size method
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Get size method
+
+        Args:
+            value: int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        get area method
+        """
+        return self.size * self.size
