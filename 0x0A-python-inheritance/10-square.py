c7f812c61d3f653b0d96c87c70957fe12185c412
mnalishuwa <mnalishuwa@gmail.com>
2023-09-11 23:33:00 +0000

    update

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -28,3 +28,21 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """
+        Return string representation of Rectangle instance
+        """
+        cls_name = self.__class__.__name__
+        return str("[{}] {}/{}".format(cls_name, self.__size, self.__size))
+
+    def area(self):
+        """
+        Area method
+
+        Args:
+
+        Return:
+            int - width * height
+        """
+        return self.__size * self.__size
