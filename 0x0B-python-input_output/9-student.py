3006a94157d4c52c513351f3c30f3a3aa7d7ef28
mnalishuwa <mnalishuwa@gmail.com>
2023-09-12 23:02:16 +0000

    Add task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+Module 9-student docs
+
+Class Student defines student object data structure
+"""
+
+
+class Student:
+    """
+    Data structure for student data
+
+    Attributes:
+        first_name - str
+        last_name - str
+        age - int, student age
+
+    Methods:
+        __init__(self, first_name, last_name, age)
+        to_json(self)
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor method
+
+        Args:
+            first_name
+            last_name
+            age
+
+        Return:
+            object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Return dictionary representation of student
+
+        Args:
+
+        Return:
+            dict
+        """
+        return {attr: getattr(self, attr) for attr in dir(self)
+                if callable(getattr(self, attr)) and not attr.startswith("__")}
