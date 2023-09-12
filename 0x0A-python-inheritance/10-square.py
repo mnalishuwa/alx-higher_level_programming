91b928550a6f2bf9087d0d1a2a34ddc30959da51
mnalishuwa <mnalishuwa@gmail.com>
2023-09-12 10:52:01 +0000

    update square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,7 +3,7 @@
 """
 10-square module docs
 
-Square class Inherits from Rectangle class
+Square class inherits from Rectangle class
 
 Rectangle class inherits from BaseGeometry
 """
@@ -14,10 +14,10 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 
 class Square(Rectangle):
     """
-    Square Class Base Blueprint
+    Square class base blueprint
 
     Attributes:
-    size (int): +ve square side
+    size (int) - +ve square side
 
     Methods:
     __init__(self, size)
@@ -39,15 +39,3 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
-
-    def __str__(self):
-        """
-        Return string representation of Rectangle instance
-
-        Arguments:
-
-        Return:
-            str
-        """
-        cls_name = self.__class__.__bases__[0].__name__
-        return str("[{:s}] {:d}/{:d}".format(cls_name, self.__size, self.__size))
