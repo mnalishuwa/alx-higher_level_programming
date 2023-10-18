02f52ea0512ecd655a94d6ad8b4ef9e32e0f52d3
mnalishuwa <mnalishuwa@gmail.com>
2023-10-18 22:39:26 +0000

    Add files

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,3 @@
 --creates a table, first_table in the current database in your MySQL server
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
