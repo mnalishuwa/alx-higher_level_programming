bac9b4aedd5bec5ca848475b4d0d1f13c3231c17
mnalishuwa <mnalishuwa@gmail.com>
2023-10-19 11:23:31 +0000

    add sql files

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,8 @@
+-- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
+ALTER database hbtn_0c_0
+DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.second_table
+DEFAULT CHARACTER SET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.second_table
+MODIFY COLUMN name VARCHAR(256)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
