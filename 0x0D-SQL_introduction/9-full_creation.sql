a019b64ccb1ffd755c7e8906f17cfeb576d19668
mnalishuwa <mnalishuwa@gmail.com>
2023-10-19 11:43:23 +0000

    Update creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS
        second_table (id INT, name VARCHAR(256), score INT);
 -- insert into created table
 INSERT INTO
-       second_table (id, name)
+       second_table (id, name, score)
 VALUES
 	(1, 'John', 10),
 	(2, 'Alex', 3),
