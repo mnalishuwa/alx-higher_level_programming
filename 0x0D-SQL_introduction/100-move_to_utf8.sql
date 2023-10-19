559bfc25f54440cd069ecc5555f0ce25c7bbf1ea
mnalishuwa <mnalishuwa@gmail.com>
2023-10-19 11:53:48 +0000

    Update creation

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,9 @@
 -- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
 ALTER database hbtn_0c_0
 DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.second_table
+USE hbtn_0c_0;
+ALTER TABLE first_table
 DEFAULT CHARACTER SET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.second_table
+ALTER TABLE first_table
 MODIFY COLUMN name VARCHAR(256)
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
