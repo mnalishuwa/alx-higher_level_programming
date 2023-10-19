ee44ba769b24fd65a7c33c79663c90c0eb6a9a2a
mnalishuwa <mnalishuwa@gmail.com>
2023-10-19 22:25:51 +0000

    Update temp3

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- only SELECT privilege in the database hbtn_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
