357a05d4eaa9486535e7ff0dfecf59803899387d
mnalishuwa <mnalishuwa@gmail.com>
2023-10-19 21:22:18 +0000

    Update temp2

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,7 +2,7 @@
 ALTER database hbtn_0c_0
 DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table
-DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY COLUMN name VARCHAR(256)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- ALTER TABLE hbtn_0c_0.first_table
+-- MODIFY COLUMN name VARCHAR(256)
+-- CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
