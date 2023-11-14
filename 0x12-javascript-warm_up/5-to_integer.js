f70865aec48dd11f42703042d9313d054daa1fcb
mnalishuwa <mnalishuwa@gmail.com>
2023-11-14 08:08:16 +0000

    update mynumber

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@
 if (isNaN(Number(process.argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(`My number is: ${Math.floor(Number(process.argv[2]))}`);
+  console.log(`My number: ${Math.floor(Number(process.argv[2]))}`);
 }
