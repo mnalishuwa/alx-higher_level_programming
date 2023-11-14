85d29256a6d0f3c9b6a8b977a91ee80b34b49692
mnalishuwa <mnalishuwa@gmail.com>
2023-11-14 08:05:12 +0000

    update

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,12 +3,12 @@
 let numArgs = 0;
 
 if (process.argv[2] === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
-    process.argv.forEach((element) => {
-	if (numArgs === 2) {
-	    console.log(`${element}`);
-	}
-	numArgs++;
-    });
+  process.argv.forEach((element) => {
+    if (numArgs === 2) {
+      console.log(`${element}`);
+    }
+    numArgs++;
+  });
 }
