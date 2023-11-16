bd9d42522548c09c5afd35442a6ead81f7fd474d
mnalishuwa <mnalishuwa@gmail.com>
2023-11-16 11:44:58 +0000

    Add task 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,24 +2,6 @@
 
 exports.converter = function (base) {
   return function (base10Number) {
-    const hexadecimal = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-
-    let conversion = '';
-    let remainder;
-
-    if (base10Number === 0) {
-      return '0';
-    }
-
-    while (base10Number > 0) {
-      remainder = base10Number % base;
-
-      if (base === 16) {
-        remainder = hexadecimal[remainder];
-      }
-      conversion = remainder.toString() + conversion;
-      base10Number = Math.trunc(base10Number / base);
-    }
-    return conversion;
+    return base10Number.toString(base);
   };
 };
