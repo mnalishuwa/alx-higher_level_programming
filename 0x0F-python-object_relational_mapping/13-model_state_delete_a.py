21006ab427dc02c8cfc55e30e5ecc9f3a7f55efd
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2023-12-11 18:48:29 +0000

    Add files 9 - 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+"""
+model_state_fetch_all - module uses python ORM to connect to database and
+select all states
+
+"""
+
+
+if __name__ == "__main__":
+    """
+    Fetch all states from named database
+    """
+    from model_state import State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker, Query
+    import sys
+    from urllib.parse import quote_plus
+
+    username = sys.argv[1]
+    password = quote_plus(sys.argv[2])
+    db_name = sys.argv[3]
+    portnumber = 3306
+
+    dbengine = create_engine("mysql+mysqldb://" +
+                             "{}:{}@localhost:{}/{}"
+                             .format(username,
+                                     password,
+                                     portnumber,
+                                     db_name),
+                             pool_pre_ping=True)
+    # Base.metadata.create_all(dbengine)
+    Session = sessionmaker(bind=dbengine)
+    session = Session()
+
+    session.query(State).\
+        filter(State.name.like("%a%")).delete(synchronize_session=False)
+    session.commit()
+
+    session.close()
