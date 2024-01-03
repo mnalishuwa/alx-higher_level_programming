d06b137b60ce064e9c783393e4925620bfdbd1ae
mnalishuwa <103501822+mnalishuwa@users.noreply.github.com>
2024-01-04 01:00:36 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-if (isNaN(Number(process.argv[2]))) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < Math.floor(Number(process.argv[2])); i++) {
-    console.log('C is fun');
-  }
-}
