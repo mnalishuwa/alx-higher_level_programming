2507f0ee1a88838d9e3cf64f1085652d665dd38e
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2024-01-26 02:29:22 +0000

    update task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL
-curl -sI $1 | grep -i '^Content-Length: ' | awk '{print $}' | tr -d '\r'
+curl -sI $1 | grep -i '^Content-Length: ' | awk '{print $}'
