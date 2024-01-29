28255b5e1dad0cb3cf218e87b9c092f89730f7e6
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2024-01-29 14:43:21 +0000

    Update 3-methods to use awk to filter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays HTTP methods the server will accept
-curl -sX OPTIONS "$1"
+curl -sI 0.0.0.0:5000/route_4 | grep 'Allow:' | awk '{print substr($0, index($0,$2))}'
