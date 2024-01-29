31dc4ae10fc19a8274a7fce37771414ff85d68df
Mukwa Nalishuwa <mnalishuwa@gmail.com>
2024-01-29 14:47:40 +0000

    Update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays HTTP methods the server will accept
-curl -sI 0.0.0.0:5000/route_4 | grep 'Allow:' | awk '{print substr($0, index($0,$2))}'
+curl -sI "$1" | grep 'Allow:' | awk '{print substr($0, index($0,$2))}'
